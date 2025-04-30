d0b644419995cd76125a409a7a08b09109399b10
unknown <vanneszias@icloud.com>
2025-04-30 12:17:17 -0700

    Init the project

---
+++ Useless Inventions/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Useless_Inventions.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
