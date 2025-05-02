80c3bb5e62b58a3a1ae55021e937519410a9056a
Zias van Nes <vanneszias@icloud.com>
2025-05-02 15:51:50 +0200

    🗃️ Feat: gitignore + db migrations

--- Useless Inventions/Data/Migrations/20250502134938_AddInventionsCommentsAndLikes.Designer.cs
+++ Useless Inventions/Data/Migrations/20250502134938_AddInventionsCommentsAndLikes.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Useless_Inventions.Data;
 
@@ -11,9 +12,11 @@ using Useless_Inventions.Data;
 namespace Useless_Inventions.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250502134938_AddInventionsCommentsAndLikes")]
+    partial class AddInventionsCommentsAndLikes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
