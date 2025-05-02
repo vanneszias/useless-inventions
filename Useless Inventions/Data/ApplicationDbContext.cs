9a91a4d9939d466b3df0ad0537b020336e399f9d
ziasvannes <ziasvannes@ziasserver>
2025-05-02 15:31:46 +0200

    Feat: add invention, like, comment props for db

--- Useless Inventions/Data/ApplicationDbContext.cs
+++ Useless Inventions/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Useless_Inventions.Models;
 
 namespace Useless_Inventions.Data
 {
@@ -8,6 +9,46 @@ namespace Useless_Inventions.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Invention> Inventions { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Like> Likes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Configure relationships and constraints
+            builder.Entity<Invention>()
+                .HasOne(i => i.User)
+                .WithMany()
+                .HasForeignKey(i => i.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Comment>()
+                .HasOne(c => c.User)
+                .WithMany()
+                .HasForeignKey(c => c.UserId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder.Entity<Comment>()
+                .HasOne(c => c.Invention)
+                .WithMany(i => i.Comments)
+                .HasForeignKey(c => c.InventionId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Like>()
+                .HasOne(l => l.User)
+                .WithMany()
+                .HasForeignKey(l => l.UserId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder.Entity<Like>()
+                .HasOne(l => l.Invention)
+                .WithMany(i => i.Likes)
+                .HasForeignKey(l => l.InventionId)
+                .OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
